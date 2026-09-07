e6489c951769f2cd17cd43c147bef08db9046295
tsunehimatoi <tsunehimatoi@gmail.com>
2026-09-07 22:19:46 +0800

    chore(release): prepare v0.5.0

--- src/main/kotlin/io/github/psd2live/core/PSD2LivePipeline.kt
+++ src/main/kotlin/io/github/psd2live/core/PSD2LivePipeline.kt
@@ -345,7 +345,7 @@ class PSD2LivePipeline {
 		{
 		  "version": 1,
 		  "model": ${quote(baseName)},
-		  "generator": "PSD2Live 0.4.0",
+		  "generator": "PSD2Live 0.5.0",
 		  "runtimeTarget": ${quote(rig.puppet.runtimeTarget.name)},
 		  "mocVersion": ${rig.puppet.runtimeTarget.mocVersion().byteValue},
 		  "canvas": {"width":${analysis.source.widthPx},"height":${analysis.source.heightPx}},
